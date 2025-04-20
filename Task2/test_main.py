1adf16470d05374e7c20f35300527306a54d719d
yogitha-koya <119508006+yogitha-koya@users.noreply.github.com>
2025-04-20 18:09:09 +0530

    test_main.py

--- Task2/test_main.py
+++ Task2/test_main.py
@@ -2,7 +2,6 @@ import pytest
 from main import calculate_average
 
 def test_calculate_average():
+    """Test cases to validate the calculate_average function."""
     assert calculate_average([10, 20, 30]) == 20
     assert round(calculate_average([1.5, 2.5, 3.0]), 2) == 2.33
-
-
